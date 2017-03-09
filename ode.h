dcdf8a2227167fc3fa46b3a5d1fa471633ea0c7f
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2017-03-09 14:57:35 -0500

    makes ode.h mixed C C++ compatible -- planning to write rk45 in C++

--- ode.h
+++ ode.h
@@ -1,11 +1,19 @@
 #ifndef INC_ODE_h // #include guard
 #define INC_ODE_h // ensure this file is only included once
 
+#ifdef __cplusplus // if this header was included in a C++ context
+extern "C" { // use C linkage. Forbids symbol mangling (and thus overloading)
+#endif // This makes it possible to cross-link C and C++ objects.
+
 #include <stdlib.h>
 
 typedef void (*derivative_function)(double t_n, double *u_n, double *out);
 
 void euler(double *u, double *u_init, int dim, int numsteps, 
            double h, double t, derivative_function get_f);
+
+#ifdef __cplusplus
+} // closing brace for extern "C"
+#endif
 
 #endif // #include guard
